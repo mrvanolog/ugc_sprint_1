9f1870954708ef799a4d43535a94ddb99e0b8f86
mrvanolog <mrvanolog97@gmail.com>
2021-04-18 22:51:18 +0300

    Add default values to ENV variables

--- etl/conf/settings.py
+++ etl/conf/settings.py
@@ -4,9 +4,9 @@ import os
 CH_HOST = os.environ.get('CH_HOST', default='localhost')
 CH_TABLE = os.environ.get('CH_TABLE', default='analysis.viewed_progress')
 
-KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC')
-KAFKA_CONSUMER_GROUP = os.environ.get('KAFKA_CONSUMER_GROUP')
-KAFKA_SERVERS = os.environ.get('KAFKA_SERVERS')
+KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC', default='views')
+KAFKA_CONSUMER_GROUP = os.environ.get('KAFKA_CONSUMER_GROUP', default='echo-messages-to-stdout')
+KAFKA_SERVERS = os.environ.get('KAFKA_SERVERS', default=['localhost:9092'])
 
 FLUSH_SECONDS = float(os.environ.get('FLUSH_SECONDS', default='30'))
 FLUSH_COUNT = int(os.environ.get('FLUSH_COUNT', default='1000'))
