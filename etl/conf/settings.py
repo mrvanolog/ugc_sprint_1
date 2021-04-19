9e869fac0856275757b6f4f161e3bfd39a8450a5
mrvanolog <mrvanolog97@gmail.com>
2021-04-19 16:40:26 +0300

    Move log conf init to settings

--- etl/conf/settings.py
+++ etl/conf/settings.py
@@ -1,12 +1,19 @@
 import os
+import yaml
+from pathlib import Path
 
 
-CH_HOST = os.environ.get('CH_HOST', default='localhost')
+CH_HOST = os.environ.get('CH_HOST', default='clickhouse://clickhouse-node1')
 CH_TABLE = os.environ.get('CH_TABLE', default='analysis.viewed_progress')
 
 KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC', default='views')
 KAFKA_CONSUMER_GROUP = os.environ.get('KAFKA_CONSUMER_GROUP', default='echo-messages-to-stdout')
-KAFKA_SERVERS = os.environ.get('KAFKA_SERVERS', default=['localhost:9092'])
+KAFKA_SERVERS = os.environ.get('KAFKA_SERVERS', default=['localhost:9092', 'broker:29092'])
 
 FLUSH_SECONDS = float(os.environ.get('FLUSH_SECONDS', default='30'))
-FLUSH_COUNT = int(os.environ.get('FLUSH_COUNT', default='1000'))
+FLUSH_COUNT = int(os.environ.get('FLUSH_COUNT', default='1'))
+
+# инициализируем конфиг для logging
+path_log_conf = Path(__file__).parent.joinpath("log_conf.yaml")
+with path_log_conf.open("r") as f:
+    log_conf = yaml.safe_load(f)
