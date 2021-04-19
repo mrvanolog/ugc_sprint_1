145d373538543ac51cc367c9a598ff0e6cc595ca
mrvanolog <mrvanolog97@gmail.com>
2021-04-19 16:40:37 +0300

    Add logging to backoff

--- etl/conf/utils.py
+++ etl/conf/utils.py
@@ -1,5 +1,12 @@
+import logging
 import time
 from functools import wraps
+
+from conf.settings import log_conf
+
+
+logging.config.dictConfig(log_conf)
+logger = logging.getLogger("backoff")
 
 
 def backoff(func):
@@ -9,13 +16,16 @@ def backoff(func):
         border_sleep_time = 10
 
         while True:
+            logger.debug('Попытка подключения к БД...')
             if t < border_sleep_time:
                 t *= 2
 
             try:
                 conn = func(*args, **kwargs)
                 return conn
-            except Exception:
+            except Exception as e:
+                logger.error(f'Ошибка подключения: {e}')
+                logger.info(f'Повторная попытка через {t} секунд')
                 time.sleep(t)
 
     return inner
