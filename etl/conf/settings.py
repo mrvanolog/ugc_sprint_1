6f87cdee95f1cb90f0ddd4ecfb34300aec2f83eb
mrvanolog <mrvanolog97@gmail.com>
2021-04-20 20:19:44 +0300

    Update FLUSH_COUNT limit to 1000

--- etl/conf/settings.py
+++ etl/conf/settings.py
@@ -11,7 +11,7 @@ KAFKA_CONSUMER_GROUP = os.environ.get('KAFKA_CONSUMER_GROUP', default='echo-mess
 KAFKA_SERVERS = os.environ.get('KAFKA_SERVERS', default=['localhost:9092', 'broker:29092'])
 
 FLUSH_SECONDS = float(os.environ.get('FLUSH_SECONDS', default='30'))
-FLUSH_COUNT = int(os.environ.get('FLUSH_COUNT', default='1'))
+FLUSH_COUNT = int(os.environ.get('FLUSH_COUNT', default='1000'))
 
 # инициализируем конфиг для logging
 path_log_conf = Path(__file__).parent.joinpath("log_conf.yaml")
